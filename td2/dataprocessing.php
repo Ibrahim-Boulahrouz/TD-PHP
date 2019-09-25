519121339017323bbeda491950cf4aae09a16467
b18007615 <ibrahim.boulahrouz@etu.univ-amu.fr>
2019-09-25 15:24:59 +0200

    dataprocessing

---
+++ td2/dataprocessing.php
@@ -0,0 +1,36 @@
+<?php
+include 'Start_end.php';
+start_page('TD2');
+?>
+
+    <form action="data-processing.php" method="post">
+        <label for="identifiant">Identifiant :</label><input id="identifiant" type="text" name="identifiant"><br/>
+
+        <label for="civ1">Homme :</label><input id="civ1" type="radio" name="civ" value="Homme"><br/>
+
+        <label for="civ2">Femme :</label><input id="civ2" type="radio" name="civ" value="Femme"><br/>
+
+        <label for="email">E-mail :</label><input id="email" type="text" name="email"><br/>
+
+        <label for="mdp">Veuillez saisir votre mot de passe :</label><input id="mdp" type="password" name="mdp"><br/>
+
+        <label for="vmdp">Veuillez confirmer votre mot de passe :</label><input id="vmdp" type="password" name="vmdp"><br/>
+
+        <label for="tel">Téléphone :</label><input id="tel" type="tel" name="tel"><br/>
+
+        <select id="select_pays">
+            <option value="">Choisir un pays</option>
+            <option value="France">France</option>
+            <option value="Angleterre">Angleterre</option>
+            <option value="Allemagne">Allemagne</option>
+        </select><br/>
+        <label for="CGU">Acceptez les CGU</label><input id="CGU" type="checkbox" name="CGU"><br/>
+
+        <label for="action">Soumettre</label><input id="action" type="submit" name="action" value="mailer"><br/>
+
+
+    </form>
+
+<?php
+end_page();
+?><?php
