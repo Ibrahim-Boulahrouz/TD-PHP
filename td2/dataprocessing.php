f3fc7b7bf24db98a4811b61445ec22cacbc164be
b18007615 <ibrahim.boulahrouz@etu.univ-amu.fr>
2019-09-25 15:39:05 +0200

    TD2 envoi mail

--- td2/dataprocessing.php
+++ td2/dataprocessing.php
@@ -1,36 +1,24 @@
 <?php
-include 'Start_end.php';
-start_page('TD2');
-?>
+include 'mutualisation.php';
+start_page('Data processing');
 
-    <form action="data-processing.php" method="post">
-        <label for="identifiant">Identifiant :</label><input id="identifiant" type="text" name="identifiant"><br/>
+$action = $_POST['action'];
+$identifiant = $_POST['identifiant'];
+$email = $_POST['email'];
+$mdp = $_POST['mdp'];
+if($action == 'mailer')
+{
+    $message = 'Voici vos identifiants d\'inscription :' . $identifiant . '<br/>';
+    $message .= 'Email : ' . $email . '<br/>';
+    $message .= 'Mot de passe : ' . $mdp . '<br/>';
+    mail('itsneyraa@outlook.fr','Inscription',$message);?>
 
-        <label for="civ1">Homme :</label><input id="civ1" type="radio" name="civ" value="Homme"><br/>
-
-        <label for="civ2">Femme :</label><input id="civ2" type="radio" name="civ" value="Femme"><br/>
-
-        <label for="email">E-mail :</label><input id="email" type="text" name="email"><br/>
-
-        <label for="mdp">Veuillez saisir votre mot de passe :</label><input id="mdp" type="password" name="mdp"><br/>
-
-        <label for="vmdp">Veuillez confirmer votre mot de passe :</label><input id="vmdp" type="password" name="vmdp"><br/>
-
-        <label for="tel">Téléphone :</label><input id="tel" type="tel" name="tel"><br/>
-
-        <select id="select_pays">
-            <option value="">Choisir un pays</option>
-            <option value="France">France</option>
-            <option value="Angleterre">Angleterre</option>
-            <option value="Allemagne">Allemagne</option>
-        </select><br/>
-        <label for="CGU">Acceptez les CGU</label><input id="CGU" type="checkbox" name="CGU"><br/>
-
-        <label for="action">Soumettre</label><input id="action" type="submit" name="action" value="mailer"><br/>
-
-
-    </form>
-
-<?php
+    Votre message a bien été envoyé ! Retour à la page d'accueil
+    <meta http-equiv="refresh" content="5; url=TD2.php">
+    <?php
+}
+else {
+    echo '<br/><strong>Bouton non géré !</strong><br/>';
+}
 end_page();
-?><?php
+?>
