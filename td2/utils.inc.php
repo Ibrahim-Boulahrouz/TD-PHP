db4c495cf53310c62e5142feb2100e9b19a47c04
b18007615 <ibrahim.boulahrouz@etu.univ-amu.fr>
2019-09-25 14:33:10 +0200

    debut td2

---
+++ td2/utils.inc.php
@@ -0,0 +1,8 @@
+<?php
+function start_page($title) {
+    echo '<!DOCTYPE html> <html> <head> <title>' . $title . '</title> </head> <body>';
+};
+function end_page() {
+    echo '</body> </html>';
+};
+?><?php
